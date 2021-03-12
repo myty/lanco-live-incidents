eea3cefd7704e32ebff5e4058eeb56fb17f0f0e8
Michael Tyson <michaeltyson@outlook.com>
2021-03-12 06:54:21 -0500

    change map marker color

--- src/lanco-incidents-app/src/components/map-marker.tsx
+++ src/lanco-incidents-app/src/components/map-marker.tsx
@@ -8,7 +8,7 @@ interface MapMarkerProps extends Partial<Coords> {
 
 export default function MapMarker({ text, primary = false }: MapMarkerProps) {
     const className = `${
-        primary ? "border-blue-900 bg-transparent" : "border-gray-500"
+        primary ? "border-blue-900 bg-transparent" : "border-blue-300"
     } rounded-full h-8 w-8 flex items-center justify-center border-4 relative -left-4 -top-4`;
 
     return (
