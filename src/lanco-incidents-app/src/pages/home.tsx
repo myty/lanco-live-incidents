da8d3f3d55ec43fa801a288b5768c84e0f70ce97
Michael Tyson <michaeltyson@outlook.com>
2021-05-10 15:19:12 -0400

    remove buggy pull to refresh

--- src/lanco-incidents-app/src/pages/home.tsx
+++ src/lanco-incidents-app/src/pages/home.tsx
@@ -1,18 +1,13 @@
 import Layout from "containers/layout";
 import IncidentsList from "components/incidents-list";
 import useIncidents from "hooks/use-incidents";
-import React, { useRef } from "react";
+import React from "react";
 import { SITE_TITLE } from "constants/app-constants";
 import RefreshButton from "components/refresh-button";
 import SettingsButton from "components/settings-button";
-import PullToRefresh from "components/pull-to-refresh";
-
-const REFRESH_TRIGGER_DISTANCE = 80;
 
 const Home: React.FC = () => {
     const { incidents, loading, refresh } = useIncidents();
-
-    const listElement = useRef<HTMLDivElement>(null);
 
     return (
         <Layout
@@ -28,15 +23,7 @@ const Home: React.FC = () => {
                     />
                 </>
             }>
-            <div
-                ref={listElement}
-                className="px-2 pt-2 text-xs overscroll-y-contain">
-                <PullToRefresh
-                    loading={loading}
-                    onRefresh={refresh}
-                    refreshTriggerDistance={REFRESH_TRIGGER_DISTANCE}
-                    watchRef={listElement}
-                />
+            <div className="px-2 pt-2 text-xs overscroll-y-contain">
                 <IncidentsList incidents={incidents} />
             </div>
         </Layout>
