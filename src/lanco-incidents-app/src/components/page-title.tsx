316698bc8f96a0a57b48bb99a1bcc719edd03a29
Michael Tyson <myty@users.noreply.github.com>
2022-11-12 15:29:51 -0500

    add incident web share (#12)

--- src/lanco-incidents-app/src/components/page-title.tsx
+++ src/lanco-incidents-app/src/components/page-title.tsx
@@ -2,13 +2,17 @@ import React, { PropsWithChildren } from "react";
 
 interface PageTitleProps {
     onBack?: () => void;
+    onShare?: () => void;
     showBackButton?: boolean;
+    showShareButton?: boolean;
 }
 
 const PageTitle: React.FC<PropsWithChildren<PageTitleProps>> = ({
     children,
     onBack,
+    onShare,
     showBackButton = true,
+    showShareButton = false,
 }) => {
     return (
         <div className="flex">
@@ -16,12 +20,14 @@ const PageTitle: React.FC<PropsWithChildren<PageTitleProps>> = ({
                 <button
                     aria-label="Go Back"
                     className="relative -left-2 w-7"
-                    onClick={() => onBack?.()}>
+                    onClick={() => onBack?.()}
+                >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke="currentColor">
+                        stroke="currentColor"
+                    >
                         <path
                             strokeLinecap="round"
                             strokeLinejoin="round"
@@ -31,7 +37,29 @@ const PageTitle: React.FC<PropsWithChildren<PageTitleProps>> = ({
                     </svg>
                 </button>
             )}
-            <div>{children}</div>
+            <div className="flex-grow">{children}</div>
+            {showShareButton && (
+                <button
+                    aria-label="Go Back"
+                    className="w-7"
+                    onClick={() => onShare?.()}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+                        />
+                    </svg>
+                </button>
+            )}
         </div>
     );
 };
