26b7b60533bb7724121c2e4a8290f21cfe314899
Michael Tyson <myty@users.noreply.github.com>
2022-12-28 11:37:53 -0500

    Add not found page and incident message (#16)

--- src/lanco-incidents-app/src/router.tsx
+++ src/lanco-incidents-app/src/router.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Home from "pages/home";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Settings from "pages/settings";
 import { IncidentDetail } from "pages/incident-detail/incident-detail";
 import AppLayout from "containers/app-layout";
+import { NotFound } from "pages/not-found";
 
 export const router = createBrowserRouter([
     {
@@ -22,6 +23,8 @@ export const router = createBrowserRouter([
                 path: "incidents/:id",
                 element: <IncidentDetail />,
             },
+            { path: "/not-found", element: <NotFound /> },
+            { path: "*", element: <Navigate to="not-found" /> },
         ],
     },
 ]);
